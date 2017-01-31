88a2b9c9c765983624c7f4e5bfcc39534c02e119
Igor Collak <FS0383@FSK.fpt.sk>
2017-01-31 18:48:36 +0100

    third commit to try auto build trigger

--- src/monitor_IceFaces2/Counter2.java
+++ src/monitor_IceFaces2/Counter2.java
@@ -52,7 +52,7 @@ public class Counter2{
 	public HttpSession session;
 	public String m_sessionId;
 	public PortableRenderer m_renderer;
-	public int increment = 27;
+	public int increment = 29;
 	private long eTime;
 	
 	boolean node1 = false;
